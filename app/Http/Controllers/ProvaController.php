a32196ed2cf6030fa3e1b083c6589ca99db0a414
angelofortunato <fortunatoangelo97@gmail.com>
2025-01-20 11:25:35 +0100

    prova inserimento sul db
    creato un prototipo di tabella prodotto e ho testato il funzionamento dell'inserimento

--- app/Http/Controllers/ProvaController.php
+++ app/Http/Controllers/ProvaController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+
 class ProvaController extends Controller
 {
     //
     public function saluto(string $nome)
     {
         return 'Ciao '.$nome;
+    }
+
+    public function inserimento()
+    {
+        $product = new Product;
+        $product->name = 'Prodotto 1';
+        $product->image = 'path immagine';
+        $product->save();
+
+        return redirect('/');
     }
 }
