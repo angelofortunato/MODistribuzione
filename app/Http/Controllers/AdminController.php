5012a09d231625489e5756249f6017901740f500
angelofortunato <fortunatoangelo97@gmail.com>
2025-01-22 00:50:04 +0100

    Aggiunte funzioni per controller Admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+
 class AdminController extends Controller
 {
     public function index()
     {
         return view('admin.index');
+    }
+
+    public function prodotti()
+    {
+        $products = Product::all();
+
+        return view('admin.prodotti', compact('products'));
+    }
+
+    public function show($id)
+    {
+        // Recupera il prodotto specifico
+        $product = Product::findOrFail($id);
+
+        // Passa il prodotto alla vista
+        return view('admin.show', compact('product'));
     }
 }
