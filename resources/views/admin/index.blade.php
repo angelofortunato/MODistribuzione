123d3cbd01aa76493be9d358cec00a0458068739
angelofortunato <fortunatoangelo97@gmail.com>
2025-01-22 00:48:28 +0100

    Aggiunto bootstrap per front-end admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,15 +10,24 @@
 	>
 	<title>Admin Dashboard</title>
 	<link
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
 		rel="stylesheet"
-		href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+		crossorigin="anonymous"
 	>
 </head>
 
 <body>
+	<script
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+		crossorigin="anonymous"
+	></script>
+	@include('admin.partials.menu')
+
 	<div class="container mt-5">
 		<h2>Admin Dashboard</h2>
-		<p>Welcome, {{ Auth::user()->name }}!</p>
+		<p class="lead">Welcome, {{ Auth::user()->name }}!</p>
 	</div>
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,12 +13,19 @@
 	>
 	<title>Homepage</title>
 	<link
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
 		rel="stylesheet"
-		href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+		crossorigin="anonymous"
 	>
 </head>
 
 <body>
+	<script
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+		crossorigin="anonymous"
+	></script>
 	<div class="container mt-5">
 		<h1 class="text-center">MODistribuzione</h1>
 		<h3 class="text-center">Sempre un passo avanti a voi</h3>
