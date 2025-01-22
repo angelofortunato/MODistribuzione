7d9effb89b19b09aca62b7d7cc8e435e704d6f25
angelofortunato <fortunatoangelo97@gmail.com>
2025-01-22 20:13:27 +0100

    modifica titolo

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -7,7 +7,7 @@
 		name="viewport"
 		content="width=device-width, initial-scale=1.0"
 	>
-	<title>Registration Form</title>
+	<title>Registrazione</title>
 	<link
 		href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
 		rel="stylesheet"
@@ -24,7 +24,7 @@
 	></script>
 
 	<div class="container mt-5">
-		<h2>Registration Form</h2>
+		<h2>Registrazione</h2>
 		@if ($errors->any())
 			<div class="alert alert-danger">
 				<ul>
